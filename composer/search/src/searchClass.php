06f8984354f85a7e3d1b5515fd812c78557f98d3
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-06-29 19:41:41 -0300

    :sparkles: feat: add class and index search courses

---
+++ composer/search/src/searchClass.php
@@ -0,0 +1,28 @@
+<?php
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Search {
+    private ClientInterface $httpClient;
+    private Crawler $crawler;
+
+    // Constructor method
+    public function __construct(ClientInterface $httpClient, Crawler $crawler) {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function search(string $url): array {
+        $result = $this->httpClient->request('GET', $url);
+        $html = $result->getBody()->getContents();
+        $this->crawler->addHtmlContent($html);
+
+        $courses = [];
+        $this->crawler->filter('span.card-curso__nome')->each(function ($node) use (&$courses) {
+            $courses[] = $node->text();
+        });
+
+        return $courses;
+    }
+}
---
+++ composer/search/src/searchCourse.php
@@ -0,0 +1,20 @@
+<?php 
+
+require '../vendor/autoload.php'; 
+require '../src/searchClass.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+// Instance clientHttp with base_url
+$client = new Client(['base_uri' => 'https://www.alura.com.br',
+'verify' => false, // Disable SSL verification
+]);
+$crawler = new Crawler();
+
+$search = new Search($client, $crawler);
+$courses = $search->search('/cursos-online-programacao/php');
+
+foreach ($courses as $course) {
+    echo $course . PHP_EOL; // Show course with line break
+}
